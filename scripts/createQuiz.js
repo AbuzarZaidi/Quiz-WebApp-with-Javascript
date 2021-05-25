4eed22937053fb0edf414f7099dc37d178de3319
AbuzarZaidi <abuzarzaidi947@gmail.com>
2021-05-24 19:04:26 -0800

    24/5/2021

--- scripts/createQuiz.js
+++ scripts/createQuiz.js
@@ -4,17 +4,17 @@ let createQuizQuestion = document.getElementById("createQuizQuestion");
 let showQuestionBtn = "";
 let QuizData = [];
 let multipleQuizData = [];
-let singleQuizArray=[];
-let addQuestionBtn="";
-let quizCreatedBtn="";
+let singleQuizArray = [];
+let addQuestionBtn = "";
+let quizCreatedBtn = "";
 // localStorage.clear();
 //on click create quiz button
-let i=1;
+let i = 1;
 createQuizBtn.addEventListener("click", (e) => {
-  i=1;
+  i = 1;
   e.preventDefault();
-  console.log("createQuiz.js",i);
-  i++
+  console.log("createQuiz.js", i);
+  i++;
   document.getElementById("starter").style.display = "none";
   showQuizInformation();
   quizQuestion();
@@ -75,19 +75,18 @@ const quizQuestion = () => {
     console.log("show Question");
     document.getElementById("createQuizInformation").style.display = "none";
     collectInfo();
-   // console.log(createdQuizData);
+    // console.log(createdQuizData);
     showQuizQuestion();
-    console.log(quizCreatedBtn,addQuestionBtn);
+    console.log(quizCreatedBtn, addQuestionBtn);
     addAnotherButton();
     quizCreatedsuccessfully();
-
   });
 };
 
 //=======================================show first quiz question
-const showQuizQuestion=()=>{
+const showQuizQuestion = () => {
   document.getElementById("createQuizQuestion").style.display = "block";
-  createQuizQuestion.innerHTML=` <div class="container ">
+  createQuizQuestion.innerHTML = ` <div class="container ">
 
   <h1>Questions-</h1>
 <br>
@@ -121,122 +120,119 @@ placeholder="option d" id="1d">
 <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
 placeholder="answer" id="1ans">
 </div>
-</div>`
-addQuestionBtn=document.getElementById('addQuestionBtn');
-quizCreatedBtn=document.getElementById('quizCreatedBtn');
-
-}
+</div>`;
+  addQuestionBtn = document.getElementById("addQuestionBtn");
+  quizCreatedBtn = document.getElementById("quizCreatedBtn");
+};
 // =================================---add question button
-const addAnotherButton=()=>{
-  addQuestionBtn.addEventListener('click',(e)=>{
-    console.log('donebutton');
-    
-    let div=document.createElement('div');
-    div.setAttribute('id',`${i}div`)
-      let div1=document.createElement('div');
-      div1.setAttribute('class','input-group input-group-lg');
-      let span1=document.createElement('span');
-      span1.setAttribute('class','input-group-text mb-3')
-     span1.innerText=`${i}`;
-     let input1=document.createElement('input');
-     input1.setAttribute('type','text');
-     input1.setAttribute('class','form-control mb-3')
-     input1.setAttribute('id',`${i}ques`);
-     div1.appendChild(span1);
-     div1.appendChild(input1);
-     div.appendChild(div1)
-     //option1
-     let div2=document.createElement('div');
-     div2.setAttribute('class','input-group input-group-sm mb-3');
-     let span2=document.createElement('span');
-      span2.setAttribute('class','input-group-text')
-     span2.innerText='a)';
-     let input2=document.createElement('input');
-     input2.setAttribute('type','text');
-     input2.setAttribute('class','form-control')
-     input2.setAttribute('id',`${i}a`);
-     div2.appendChild(span2);
-      div2.appendChild(input2);
-      div.appendChild(div2);
-     //option2
-     let div3=document.createElement('div');
-     div3.setAttribute('class','input-group input-group-sm mb-3');
-     let span3=document.createElement('span');
-      span3.setAttribute('class','input-group-text')
-     span3.innerText='b)';
-     let input3=document.createElement('input');
-     input3.setAttribute('type','text');
-     input3.setAttribute('class','form-control')
-     input3.setAttribute('id',`${i}b`);
-     div3.appendChild(span3);
-      div3.appendChild(input3);
-      div.appendChild(div3);
-     //option3
-     let div4=document.createElement('div');
-     div4.setAttribute('class','input-group input-group-sm mb-3');
-     let span4=document.createElement('span');
-      span4.setAttribute('class','input-group-text')
-     span4.innerText='c)';
-      let input4=document.createElement('input');
-     input4.setAttribute('type','text');
-     input4.setAttribute('class','form-control')
-     input4.setAttribute('id',`${i}c`);
-     div4.appendChild(span4);
-      div4.appendChild(input4);
-      div.appendChild(div4)
-     //option4
-     let div5=document.createElement('div');
-     div5.setAttribute('class','input-group input-group-sm mb-3');
-     let span5=document.createElement('span');
-      span5.setAttribute('class','input-group-text')
-     span5.innerText='d)';
-     let input5=document.createElement('input');
-     input5.setAttribute('type','text');
-     input5.setAttribute('class','form-control')
-     input5.setAttribute('id',`${i}d`);
-     div5.appendChild(span5);
-      div5.appendChild(input5);
-      div.appendChild(div5);
-       //answer
-     let div6=document.createElement('div');
-     div6.setAttribute('class','input-group input-group-sm mb-3');
-     let span6=document.createElement('span');
-      span6.setAttribute('class','input-group-text')
-     span6.innerText='ans)';
-     let input6=document.createElement('input');
-     input6.setAttribute('type','text');
-     input6.setAttribute('class','form-control')
-     input6.setAttribute('id',`${i}ans`);
-     div6.appendChild(span6);
-      div6.appendChild(input6);
-      div.appendChild(div6);
-     document.getElementById("createQuizQuestion").appendChild(div);
-     i++;
-  })
-}
+const addAnotherButton = () => {
+  addQuestionBtn.addEventListener("click", (e) => {
+    console.log("donebutton");
 
-
+    let div = document.createElement("div");
+    div.setAttribute("id", `${i}div`);
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "input-group input-group-lg");
+    let span1 = document.createElement("span");
+    span1.setAttribute("class", "input-group-text mb-3");
+    span1.innerText = `${i}`;
+    let input1 = document.createElement("input");
+    input1.setAttribute("type", "text");
+    input1.setAttribute("class", "form-control mb-3");
+    input1.setAttribute("id", `${i}ques`);
+    div1.appendChild(span1);
+    div1.appendChild(input1);
+    div.appendChild(div1);
+    //option1
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "input-group input-group-sm mb-3");
+    let span2 = document.createElement("span");
+    span2.setAttribute("class", "input-group-text");
+    span2.innerText = "a)";
+    let input2 = document.createElement("input");
+    input2.setAttribute("type", "text");
+    input2.setAttribute("class", "form-control");
+    input2.setAttribute("id", `${i}a`);
+    div2.appendChild(span2);
+    div2.appendChild(input2);
+    div.appendChild(div2);
+    //option2
+    let div3 = document.createElement("div");
+    div3.setAttribute("class", "input-group input-group-sm mb-3");
+    let span3 = document.createElement("span");
+    span3.setAttribute("class", "input-group-text");
+    span3.innerText = "b)";
+    let input3 = document.createElement("input");
+    input3.setAttribute("type", "text");
+    input3.setAttribute("class", "form-control");
+    input3.setAttribute("id", `${i}b`);
+    div3.appendChild(span3);
+    div3.appendChild(input3);
+    div.appendChild(div3);
+    //option3
+    let div4 = document.createElement("div");
+    div4.setAttribute("class", "input-group input-group-sm mb-3");
+    let span4 = document.createElement("span");
+    span4.setAttribute("class", "input-group-text");
+    span4.innerText = "c)";
+    let input4 = document.createElement("input");
+    input4.setAttribute("type", "text");
+    input4.setAttribute("class", "form-control");
+    input4.setAttribute("id", `${i}c`);
+    div4.appendChild(span4);
+    div4.appendChild(input4);
+    div.appendChild(div4);
+    //option4
+    let div5 = document.createElement("div");
+    div5.setAttribute("class", "input-group input-group-sm mb-3");
+    let span5 = document.createElement("span");
+    span5.setAttribute("class", "input-group-text");
+    span5.innerText = "d)";
+    let input5 = document.createElement("input");
+    input5.setAttribute("type", "text");
+    input5.setAttribute("class", "form-control");
+    input5.setAttribute("id", `${i}d`);
+    div5.appendChild(span5);
+    div5.appendChild(input5);
+    div.appendChild(div5);
+    //answer
+    let div6 = document.createElement("div");
+    div6.setAttribute("class", "input-group input-group-sm mb-3");
+    let span6 = document.createElement("span");
+    span6.setAttribute("class", "input-group-text");
+    span6.innerText = "ans)";
+    let input6 = document.createElement("input");
+    input6.setAttribute("type", "text");
+    input6.setAttribute("class", "form-control");
+    input6.setAttribute("id", `${i}ans`);
+    div6.appendChild(span6);
+    div6.appendChild(input6);
+    div.appendChild(div6);
+    document.getElementById("createQuizQuestion").appendChild(div);
+    i++;
+  });
+};
 
 //===================================---done button
-const quizCreatedsuccessfully=(e)=>{
-  quizCreatedBtn.addEventListener("click",(e)=>{
-    console.log('done');
-    for(let j=1;j<i;j++){
-      let ques=document.getElementById(`${j}ques`).value;
-      let a=document.getElementById(`${j}a`).value;
-      let b=document.getElementById(`${j}b`).value;
-      let c=document.getElementById(`${j}c`).value;
-      let d=document.getElementById(`${j}d`).value;
-      const obj={ques,a,b,c,d};
+const quizCreatedsuccessfully = (e) => {
+  quizCreatedBtn.addEventListener("click", (e) => {
+    console.log("done");
+    for (let j = 1; j < i; j++) {
+      let ques = document.getElementById(`${j}ques`).value;
+      let a = document.getElementById(`${j}a`).value;
+      let b = document.getElementById(`${j}b`).value;
+      let c = document.getElementById(`${j}c`).value;
+      let d = document.getElementById(`${j}d`).value;
+      let ans = document.getElementById(`${j}ans`).value;
+      const obj = { ques, a, b, c, d, ans };
       singleQuizArray.push(obj);
-  
     }
     console.log(singleQuizArray);
-    localStorage.setItem("quizArray",JSON.stringify(singleQuizArray));
-   // multipleQuizData.push( singleQuizArray);
-   // console.log(multipleQuizData);
-    
+    localStorage.setItem("quizArray", JSON.stringify(singleQuizArray));
+    // multipleQuizData.push( singleQuizArray);
+    // console.log(multipleQuizData);
+
     document.getElementById("createQuizQuestion").style.display = "none";
-   document.getElementById("starter").style.display = "block";
-  })
-}
+    document.getElementById("starter").style.display = "block";
+  });
+};
--- scripts/startQuiz.js
+++ scripts/startQuiz.js
@@ -1,157 +1,179 @@
-let startQuizBtn= document.getElementById('startQuizBtn');
-let startQuizQuestion=document.getElementById('startQuizQuestion');
-let inputId="";
-let inputPassword="";
-let nextQuestion="";
-let quiz="";
-let incre=1;
-let quizArray=localStorage.getItem("quizArray");
-if(quizArray==null){
-    notesObj=[];
+let startQuizBtn = document.getElementById("startQuizBtn");
+let startQuizQuestion = document.getElementById("startQuizQuestion");
+let inputId = "";
+let inputPassword = "";
+let nextQuestion = "";
+let quiz = "";
+let incre = 1;
+let correct = 0;
+// let optionA="";
+// let optionB="";
+// let optionC="";
+// let optionD="";
+let quizArray = localStorage.getItem("quizArray");
+if (quizArray == null) {
+  notesObj = [];
+} else {
+  quiz = JSON.parse(quizArray);
 }
-else{
-    quiz=JSON.parse(quizArray);
-}
-startQuizBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log('startQuiz.js');
-     document.getElementById('starter').style.display = 'none';
-     checkCredentials();
-     inputData();
-     
-     console.log(quiz);
-     
-})
+startQuizBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("startQuiz.js");
+  document.getElementById("starter").style.display = "none";
+  checkCredentials();
+  inputData();
+
+  console.log(quiz);
+});
 
 //use to check id and password
-const checkCredentials=()=>{
-    let div=document.createElement('div');
-    div.setAttribute('id','credentials');
-    div.setAttribute('class',' container w-25 p-3')
-    //h2
-    let h2=document.createElement('h2');
-    h2.innerText='Start Quiz:';
-   // h2.setAttribute('class',' container w-25 p-3');
+const checkCredentials = () => {
+  let div = document.createElement("div");
+  div.setAttribute("id", "credentials");
+  div.setAttribute("class", " container w-25 p-3");
+  //h2
+  let h2 = document.createElement("h2");
+  h2.innerText = "Start Quiz:";
+  // h2.setAttribute('class',' container w-25 p-3');
 
-    //id
-    let div1=document.createElement('div');
-    div1.setAttribute('class','mb-3');
-    let label1=document.createElement('label');
-    label1.setAttribute('class','form-label')
-    label1.innerText='ID';
-    let input1=document.createElement('input');
-    input1.setAttribute('type','text');
-    input1.setAttribute('class','form-control');
-    input1.setAttribute('id','idCheck');
-    div1.appendChild(label1);
-    div1.appendChild(input1);
-    //password
-    let div2=document.createElement('div');
-    div2.setAttribute('class','mb-3');
-    let label2=document.createElement('label');
-    label2.setAttribute('class','form-label')
-    label2.innerText='Password';
-    let input2=document.createElement('input');
-    input2.setAttribute('type','text');
-    input2.setAttribute('class','form-control');
-    input2.setAttribute('id','passwordCheck');
-    div2.appendChild(label2);
-    div2.appendChild(input2);
-    div.appendChild(h2);
-    div.appendChild(div1);
-    div.appendChild(div2);
-    //button
-    let button=document.createElement('button')
-    button.innerText='Submit';
-    button.setAttribute('id','submitBtn');
-    button.setAttribute('class','btn btn-primary');
-    div.appendChild(button);
-    // document.body.appendChild(h2);
-    document.getElementById("startQuizCredentials").appendChild(div);
-}
-const inputData=()=>{
-let submitBtn=document.getElementById('submitBtn');
-submitBtn.addEventListener('click',(e)=>{
-    document.getElementById('credentials').style.display = 'none';
-     inputId=document.getElementById('idCheck').value;
-    inputPassword=document.getElementById('passwordCheck').value;
-    console.log(inputId,inputPassword);
-showQuestion();
+  //id
+  let div1 = document.createElement("div");
+  div1.setAttribute("class", "mb-3");
+  let label1 = document.createElement("label");
+  label1.setAttribute("class", "form-label");
+  label1.innerText = "ID";
+  let input1 = document.createElement("input");
+  input1.setAttribute("type", "text");
+  input1.setAttribute("class", "form-control");
+  input1.setAttribute("id", "idCheck");
+  div1.appendChild(label1);
+  div1.appendChild(input1);
+  //password
+  let div2 = document.createElement("div");
+  div2.setAttribute("class", "mb-3");
+  let label2 = document.createElement("label");
+  label2.setAttribute("class", "form-label");
+  label2.innerText = "Password";
+  let input2 = document.createElement("input");
+  input2.setAttribute("type", "text");
+  input2.setAttribute("class", "form-control");
+  input2.setAttribute("id", "passwordCheck");
+  div2.appendChild(label2);
+  div2.appendChild(input2);
+  div.appendChild(h2);
+  div.appendChild(div1);
+  div.appendChild(div2);
+  //button
+  let button = document.createElement("button");
+  button.innerText = "Submit";
+  button.setAttribute("id", "submitBtn");
+  button.setAttribute("class", "btn btn-primary");
+  div.appendChild(button);
+  // document.body.appendChild(h2);
+  document.getElementById("startQuizCredentials").appendChild(div);
+};
+const inputData = () => {
+  let submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", (e) => {
+    document.getElementById("credentials").style.display = "none";
+    inputId = document.getElementById("idCheck").value;
+    inputPassword = document.getElementById("passwordCheck").value;
+    console.log(inputId, inputPassword);
 
-nextQuestionFunc();
-// if(singleQuizArray[0].quizId==inputId && singleQuizArray[0].quizPassword==inputPassword)
-// {
-    
-// }
-// else{
-//     console.log('wrong entry');
-    
-// }
-// ${singleQuizArray[i].ques}
-})
-}
-const showQuestion=()=>{
-    startQuizQuestion.innerHTML="";
-    document.getElementById('starter').style.display = 'none';
-    startQuizQuestion.innerHTML+=`<div class="card " style="width: 50rem;">
+    if (
+      singleQuizArray[0].quizId == inputId &&
+      singleQuizArray[0].quizPassword == inputPassword
+    ) {
+      showQuestion();
+      nextQuestionFunc();
+    } else {
+      console.log("wrong entry");
+      checkCredentials();
+      inputData();
+    }
+    // ${singleQuizArray[i].ques}
+  });
+};
+const showQuestion = () => {
+  startQuizQuestion.innerHTML = "";
+  document.getElementById("starter").style.display = "none";
+  startQuizQuestion.innerHTML += `
+    <h3 >${singleQuizArray[0].quizTitle}</h3>
+    <p >${singleQuizArray[0].quizDescription}</p>
+    `;
+  startQuizQuestion.innerHTML += `<div class="card " style="width: 50rem;">
+
     <div class="card-body" >
       <h6 class="card-title">${incre})${singleQuizArray[incre].ques}</h6>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-        <label class="form-check-label" for="flexRadioDefault1">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="optionA">
+        <label class="form-check-label" for="flexRadioDefault1" id="optionAV">
           ${singleQuizArray[incre].a}
         </label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="optionB">
+        <label class="form-check-label" for="flexRadioDefault2" id="optionBV">
         ${singleQuizArray[incre].b}
         </label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="optionC">
+        <label class="form-check-label" for="flexRadioDefault3" id="optionCV">
         ${singleQuizArray[incre].c}
         </label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="optionD" >
+        <label class="form-check-label" for="flexRadioDefault4" id="optionDV">
        ${singleQuizArray[incre].d}
         </label>
       </div>
       <button  type="submit" class="btn btn-primary" id="nextQuestion">Next</button>
     </div>
 
-  </div>`
+  </div>`;
 
-   nextQuestion=document.getElementById('nextQuestion');
-  console.log(nextQuestion);
- 
+  nextQuestion = document.getElementById("nextQuestion");
+
   incre++;
-}
-
+};
+let j = 1;
 // singleQuizArray
-const nextQuestionFunc=()=>{
-    nextQuestion.addEventListener('click',(e)=>{
-        if(incre<singleQuizArray.length)
-        {
-            showQuestion();
-            nextQuestionFunc();
-        }
-        
-else{
-    console.log('not working');
-    
-}
-        
-       
-    })
-}
-        //startQuizQuestion.innerHTML="";
-       
-     //   console.log(incre);
-        
-        
-        
-  
+const nextQuestionFunc = () => {
+  nextQuestion.addEventListener("click", (e) => {
+    console.log(`Arrayans ${singleQuizArray[j].ans}`);
+
+    let optionA = document.getElementById("optionA");
+    let optionB = document.getElementById("optionB");
+    let optionC = document.getElementById("optionC");
+    let optionD = document.getElementById("optionD");
+    let value = "";
+    if (optionA.checked) {
+      value = document.getElementById("optionAV").innerText;
+      console.log(value);
+    } else if (optionB.checked) {
+      value = document.getElementById("optionBV").innerText;
+      console.log(value);
+    } else if (optionC.checked) {
+      value = document.getElementById("optionCV").innerText;
+      console.log(value);
+    } else if (optionD.checked) {
+      value = document.getElementById("optionDV").innerText;
+      console.log(value);
+    }
+    if (value == singleQuizArray[j].ans) {
+      correct++;
+    }
+    if (incre < singleQuizArray.length) {
+      showQuestion();
+      nextQuestionFunc();
+    } else {
+      console.log(correct);
+      startQuizQuestion.innerHTML += `<h3> you got ${correct} out of ${
+        incre - 1
+      } <h3>`;
+    }
+    j++;
+  });
+};
